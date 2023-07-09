1873c2ec4a12bde0332cf91effeecd20cebbe42f
Rubanov <Rubanov.work@gmail.com>
2023-07-09 10:19:28 +0300

    initial commit

---
+++ src/main/java/pro/sky/java/course2/hw25collections/Hw25CollectionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hw25collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw25CollectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw25CollectionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/hw25collections/Hw25CollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hw25collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw25CollectionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
